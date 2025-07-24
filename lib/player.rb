fb9eeb5c635bcd21f38b92e2ea6e562361d676b6
atulvishw240 <atulvishw240@gmail.com>
2025-07-24 13:38:33 +0530

    Split extra responsibilities from Player#make_move

--- lib/player.rb
+++ lib/player.rb
@@ -12,9 +12,14 @@ class Player
   def make_move
     print "Enter your move in the form (row,col): "
     move = gets.chomp.split(",").map(&:to_i)
-    valid_move = move.all? { |index| index.between?(0, 2) }
-    make_move unless valid_move
+    make_move unless valid_move?(move)
 
     Position.new(move[0], move[1])
+  end
+
+  private
+
+  def valid_move?(move)
+    move.all? { |index| index.between?(0, 2) }
   end
 end
