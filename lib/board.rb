5afd76e8f2e76344071c3809f48c76b3d4bfde9d
atulvishw240 <atulvishw240@gmail.com>
2025-07-24 11:48:01 +0530

    Hide internal state of Board
    Provide public methods to tell Board what you want from it, instead of
    directly referring to the board array as board[0][1]. This way if the
    structure changes from array to hash in future then we can still support
    it and change will only be required in the board class and not all class
    that depends upon it.

--- lib/board.rb
+++ lib/board.rb
@@ -1,114 +1,39 @@
 # Create our board for game
 class Board
-  attr_accessor :board, :current_player_id, :no_of_turns
+  private attr_reader :board
 
-  def initialize(player1, player2)
+  def initialize
     @board = Array.new(3) { Array.new(3, " ") }
-    @players = [player1, player2]
-    @current_player_id = 0
-    @no_of_turns = 0
-
-    puts "#{player1.name} is #{player1.symbol} and #{player2.name} is #{player2.symbol} "
   end
 
-  def play
-    display
-    i = 0
-    while i < 9
-      move = current_player.make_move
-      update_board(current_player, move)
-      if won?(current_player)
-        winning_message(current_player)
-        break
-      end
-
-      switch_players!
-      i += 1
-    end
-
-    draw
+  def mark_cell(position, marker)
+    board[position.row][position.col] = marker
   end
 
-  #-----------------x ALL PRIVATE METHODS ARE BELOW  ----------------x
-  # private
-
-  def current_player
-    @players[current_player_id]
+  def cell_empty?(position)
+    board[position.row][position.col].nil?
   end
 
-  def switch_players!
-    self.current_player_id = 1 - current_player_id
+  def cell_value(position)
+    board[position.row][position.col]
   end
 
-  def update_board(player, move)
-    row = move[0].to_i
-    col = move[1].to_i
-    board[row][col] = player.symbol
-    self.no_of_turns += 1
-    display
+  def full?
+    board.flatten.none?(&:nil?)
   end
 
-  def display
-    puts "    0   1   2  "
-    board.each_with_index do |row, row_index|
-      puts "  -------------"
-      print "#{row_index} "
-      row.each_index do |col_index|
-        print "| #{@board[row_index][col_index]} "
-      end
-
-      print "|\n"
-    end
-
-    puts "  -------------"
+  def rows
+    board
   end
 
-  def row_match?(player)
-    board.any? do |row|
-      row.all? { |marker| marker == player.symbol }
-    end
+  def columns
+    board.transpose
   end
 
-  def col_match?(player)
-    col_index = 0
-    while col_index < 3
-      row_index = 0
-      counter = 0
-      while row_index < 3
-        counter += 1 if board[row_index][col_index] == player.symbol
-
-        row_index += 1
-      end
-
-      return true if counter == 3
-
-      col_index += 1
-    end
-  end
-
-  def diagonal_match?(player)
-    diagonal_winning_moves = [[[0, 0], [1, 1], [2, 2]], [[0, 2], [1, 1], [2, 0]]]
-
-    diagonal_winning_moves.any? do |diagonal|
-      diagonal.all? do |coord|
-        row_index = coord[0]
-        col_index = coord[1]
-        board[row_index][col_index] == player.symbol
-      end
-    end
-  end
-
-  def draw
-    return unless no_of_turns == 9
-
-    puts "The Game ended in Draw!!"
-  end
-
-  def won?(player)
-    col_match?(player) || row_match?(player) || diagonal_match?(player)
-  end
-
-  def winning_message(player)
-    puts "#{player.name} won the Game!!"
+  def diagonals # rubocop:disable Metrics/AbcSize
+    [
+      [board[0][0], board[1][1], board[2][2]],
+      [board[0][2], board[1][1], board[2][0]]
+    ]
   end
 end
